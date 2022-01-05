0fc7c94b64badc2c21d332c3b8e120efd25ee4e3
Mark <markloercher@gmail.com>
2022-01-05 14:09:30 -0500

    added handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
